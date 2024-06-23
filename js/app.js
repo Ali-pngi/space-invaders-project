e3cc36a750d2768ca0aabd2c5cf5fa4deca595aa
Ali <belitherali@gmail.com>
2024-06-23 17:05:58 +0100

    reduced container size and corrected syntax to target the container better

--- js/app.js
+++ js/app.js
@@ -4,61 +4,35 @@
   let win;
   let tie;
 
-
-
-
   const highScoreEl = document.getElementById('high-score');
   const resetBtnEl= document.getElementById('reset');
   
   
   // Container size 
-  const width = 50
-  const height = 50
+  const width = 25
+  const height = 25
   const totalSquareCount = width * height
-  const grid = document.querySelector('.grid')
+  const container = document.querySelector('.container')
   const squareEls = []
+  
+  
 
+                  
+                  
 
-Init();
-
-
-function init() {
-    container =
-    player = 
-    invaders = 
-    winner = false
-    lose = false
-
-    render();
-
-}
-
-
-function render (){
-    updateContainer();
-    updateMessage();
-}
-
-
-function updateContainer() {
-    container.foreach((value, index) => {
-        const grid = gridEls[index];
-        grid.textContent = value;
-    });
-}
-
-
-// container creation 
-for (let i = 0; i <totalSquareCount; i++){
-    const square = document.createElement('div')
-    square.innerText = i
-    square.classList.add('sqr')
-    square.id = i
-
-
-    square.style.height = `${100 / height}%`
-    square.style.width = `${100 / width}%`
-
-    squareEls.push(square)
-    grid.appendChild(square)
-}
+  // container creation 
+  for (let i = 0; i <totalSquareCount; i++){
+    const square = document.createElement('div');
+    square.innerText = i;
+    square.classList.add('sqr');
+    square.id = i;
+    
+    
+    square.style.height = `${100 / height}%`;
+    square.style.width = `${100 / width}%`;
+    
+    squareEls.push(square);
+    container.appendChild(square); 
+    }
+    
+    console.log(square);
