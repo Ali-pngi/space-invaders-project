e10f4d14f1a1f21ccbe2c97b7321186b51b2faf0
Ali <belitherali@gmail.com>
2024-06-24 13:03:52 +0100

    added assets for player ship and implemented player grid movement

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Friday 21st June 2024
 - created wire-frame, user stories & psuedocode. Project approved and repo created.
+
+
+Saturday 22ND June 2024
+- created code for container and square generation, along  with basic css 
+
+sunday 23rd june 2024
+
+- corrected syntax errors in html and js, got container & squares functioning correctly. added some more styling in css (mostly alignment)
+
+Monday 24th June 2024
+
+- began work on player element and implementing grid movement 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
   
-
-
   let win;
   let tie;
 
@@ -9,8 +7,8 @@
   
   
   // Container size 
-  const width = 25
-  const height = 25
+  const width = 35
+  const height = 35
   const totalSquareCount = width * height
   const container = document.querySelector('.container')
   const squareEls = []
@@ -23,7 +21,7 @@
   // container creation 
   for (let i = 0; i <totalSquareCount; i++){
     const square = document.createElement('div');
-    square.innerText = i;
+    // square.innerText = i;
     square.classList.add('sqr');
     square.id = i;
     
@@ -35,4 +33,49 @@
     container.appendChild(square); 
     }
     
-    console.log(square);
+    console.log(squareEls);
+
+
+    // Player initialisation
+    let playerPosition = 0;
+    squareEls[playerPosition].classList.add('player');
+
+    // Handle player movement 
+    function handleMove(evt) {
+        squareEls[playerPosition].classList.remove('player');
+        if (evt.key === 'ArrowLeft' && playerPosition % width !==0) {
+            playerPosition -= 1;
+        } else if (evt.key === 'ArrowRight' && playerPosition % width !== width - 1){
+            playerPosition += 1;
+        }
+        squareEls[playerPosition].classList.add('player');
+    }
+
+    // Handle projectile firing
+
+    function handleFire(evt) {
+        if (evt.key === ' ') {
+            let projectilePosition = playerPosition - width;
+
+            function moveProjectile() {
+                if (projectilePosition < 0) {
+                    clearInterval(projectileInterval);
+                    return;
+                }
+            squareEls [projectilePosition].classList.remove('projectile');
+            projectilePosition -= width;
+
+            if (projectilePosition >= 0) {
+                squareEls[projectilePosition].classList.add('projectile');
+            }
+            }
+            const projectileInterval = setInterval(moveProjectile, 100);
+
+        }
+    }
+
+
+
+    document.addEventListener('keydown', handleMove);
+    document.addEventListener('keydown', handleFire);
+
